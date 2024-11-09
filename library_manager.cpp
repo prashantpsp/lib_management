faaa54ca54aaffa694f6c0b8d2ea78c5172775a2
prashantpsp <prashantpsp.77k@gmail.com>
2024-11-09 21:26:57 +0530

    lib 1.1

--- library_manager.cpp
+++ library_manager.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include<string>
 #include<algorithm>
+#include<fstream>
+#include<sstream>
 
 using namespace std;
 string toLower(string);
@@ -52,6 +54,70 @@ class Library{
     public:
         void addBook(const Book& book){
             books.push_back(book);
+        }
+        bool isBookInFile(Book& book,const string& filename){
+            bool status=false;
+            ifstream infile(filename);
+            if(infile.is_open()){
+                string line;
+                while(getline(infile,line)){
+                    stringstream ss(line);
+                    string isbn;
+                    int i=3;
+                    while(i>0){
+                        getline(ss,isbn,',');
+                        i--;
+                    }
+                    if(book.getISBN()==isbn){
+                        status=true;
+                        break;
+                    }
+                }
+                infile.close();
+            }
+            return status;
+        }
+        void saveBooksToFile(const string& filename){
+            ofstream outfile(filename,ios_base::app);
+            if(outfile.is_open()){
+                for(auto book:books){
+                    if(isBookInFile(book,filename)){
+                        cout<<book.getTitle()<<" is stored already in file."<<endl;
+                        continue;
+                    }
+                    outfile<<book.getTitle()<<","<<book.getAuthor()<<","<<book.getISBN()<<","<<book.availability()<<endl;
+                }
+                outfile.close();
+                cout<<"Books data saved successfully in file."<<endl;
+            }
+            else{
+                cout<<"unable to open file for writing"<<endl;
+            }
+        }
+        void loadBooksFromFile(const string& filename){
+            ifstream infile(filename);
+            if(infile.is_open()){
+                books.clear();
+                string line;
+                while(getline(infile,line)){
+                    stringstream ss(line);
+                    string title,author,ISBN,avail;
+                    bool isavail;
+                    getline(ss,title,',');
+                    getline(ss,author,',');
+                    getline(ss,ISBN,',');
+                    getline(ss,avail,',');
+                    isavail = (avail=="1");
+                    Book book;
+                    book.setBook(author,title,ISBN,isavail);
+                    books.push_back(book);
+                }
+                infile.close();
+                cout<<"Books data loaded successfully."<<endl;
+            }
+            else{
+                cout<<"file could not opened"<<endl;
+            }
         }
         void displayBooks(){
             for(auto book: books){
@@ -140,15 +206,18 @@ int main(){
     b1.setBook(author, title, ISBN, is_available);
     Library l1;
     l1.addBook(b1);
+    l1.saveBooksToFile("file1.txt");
+    l1.loadBooksFromFile("file1.txt");
+    l1.displayBooks();
     //l1.removeBook(b1);
     //l1.displayBooks();
-    Book *b=l1.searchBookByauthor("saurabh shukla");
-    b->showBook();
-    User u1;
-    u1.setUser("101","saurabh");
-    u1.borrowBook(b1);
-    u1.showBorrowedBooks();
-    u1.returnBook(b1);
-    u1.showBorrowedBooks();
+    // Book *b=l1.searchBookByauthor("saurabh shukla");
+    // b->showBook();
+    // User u1;
+    // u1.setUser("101","saurabh");
+    // u1.borrowBook(b1);
+    // u1.showBorrowedBooks();
+    // u1.returnBook(b1);
+    // u1.showBorrowedBooks();
     return 0;
 }
